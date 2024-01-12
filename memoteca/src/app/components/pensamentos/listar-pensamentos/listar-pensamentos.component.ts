4be10abbe9b5acb7d6caec78c02ea46da896a2a1
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-01-12 16:34:32 -0300

    JSON server - Backend

--- memoteca/src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ memoteca/src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentosComponent implements OnInit {
   listaPensamentos = [
     {
-      conteudo: 'i love angular',
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."' ,
       autoria: 'nay',
       modelo: 'modelo3',
     },
@@ -22,6 +22,11 @@ export class ListarPensamentosComponent implements OnInit {
       autoria: 'Dev2',
       modelo: 'modelo2',
     },
+    {
+      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."' ,
+      autoria: 'nay',
+      modelo: 'modelo3',
+    }
   ];
 
   constructor() {}
