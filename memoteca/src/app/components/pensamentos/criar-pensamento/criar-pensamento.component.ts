8b95807b6c542f35905b25a75ec34537b7a83ad1
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-01-15 15:26:04 -0300

    Mensagens de erro

--- memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,7 @@ import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -16,12 +16,14 @@ export class CriarPensamentoComponent implements OnInit {
     private formBuilder: FormBuilder
   ) { }
 
+  formulario!: FormGroup;
+
   ngOnInit(): void {
     //Form builder -> Construtor de formularios
     this.formulario = this.formBuilder.group({
       //Atributos do formulario
-      conteudo: ['Formulario Reativo'],
-      autoria:['Angular'],
+      conteudo: ['', Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria:['', Validators.compose([Validators.required, Validators.minLength(3)])],
       modelo: ['modelo1']
     })
 
@@ -33,13 +35,21 @@ export class CriarPensamentoComponent implements OnInit {
     })*/
 
   }
-
-  formulario!: FormGroup;
+  habilitarBotao(): string {
+    if(this.formulario.valid){
+      return 'botao'
+    }else{
+      return 'botao__desabilitado'
+    }
+  }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelar() {
