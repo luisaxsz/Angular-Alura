4e7800b137141291a33a2f0305b1281343e3ef55
Maria Luisa Lameirão Sousa <marialuisalameirao@gmail.com>
2024-01-15 14:13:58 -0300

    Formulários reativos

--- memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,24 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
-
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
-
-
-  constructor(
+   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    //Form builder -> Construtor de formularios
+    this.formulario = this.formBuilder.group({
+      //Atributos do formulario
+      conteudo: ['Formulario Reativo'],
+      autoria:['Angular'],
+      modelo: ['modelo1']
+    })
+
+    //Outra opção
+    /*this.formulario = new FormGroup({
+      conteudo: new FormControl(''),
+      autoria: new FormControl(''),
+      modelo: new FormControl('')
+    })*/
+
   }
 
+  formulario!: FormGroup;
+
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
+    this.service.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
   }
